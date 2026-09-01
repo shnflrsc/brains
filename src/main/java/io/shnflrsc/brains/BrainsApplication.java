6111d899ec7730e6d49016a1884671dbe9dd375a
Darl Shanen Floresca <floresca.shanegarcia@gmail.com>
2026-09-02 03:18:32 +0800

    Initial commit

---
+++ src/main/java/io/shnflrsc/brains/BrainsApplication.java
@@ -0,0 +1,13 @@
+package io.shnflrsc.brains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/shnflrsc/brains/BrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shnflrsc.brains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
