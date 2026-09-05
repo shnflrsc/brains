2b8b77e3f3f58a079ed2ddeee4b2b5581f39f275
Darl Shanen Floresca <floresca.shanegarcia@gmail.com>
2026-09-05 10:42:22 +0800

    test: configure testcontainers for postgresql application context tests

--- src/test/java/io/shnflrsc/brains/BrainsApplicationTests.java
+++ src/test/java/io/shnflrsc/brains/BrainsApplicationTests.java
@@ -2,12 +2,21 @@ package io.shnflrsc.brains;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BrainsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18.6-alpine");
+
+    @Test
+    void contextLoads() {
+    }
 
 }
